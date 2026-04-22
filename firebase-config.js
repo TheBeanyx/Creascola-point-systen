e41a535daa642d2a9b9e6c02a34b334562f85e0f
szuper1212865 <buzas.benedek.iskola@gmail.com>
2026-04-22 20:52:08 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxJnZn77x2LUPORIW8nxvQqLE2Qg95aUs",
+  authDomain: "creascola-leaderboard.firebaseapp.com",
+  databaseURL: "https://creascola-leaderboard-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "creascola-leaderboard",
+  storageBucket: "creascola-leaderboard.appspot.com",
+  messagingSenderId: "227185192602",
+  appId: "1:227185192602:web:e1a6953c07ac3aa888f923"
+};
